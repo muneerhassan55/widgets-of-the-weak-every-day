925329814045b92523c5a62a4cbd8dde426c1563
muneerhassan55 <muneerhassan5544@gmail.com>
2024-07-26 16:35:43 +0500

    added  url launcher code

--- lib/screens/url_launcher.dart
+++ lib/screens/url_launcher.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class UrlLauncher extends StatefulWidget {
   const UrlLauncher({super.key});
@@ -14,6 +15,37 @@ class _UrlLauncherState extends State<UrlLauncher> {
       appBar: AppBar(
         centerTitle: true,
         title: Text("Url Launcher"),
+      ),
+      body: SizedBox.expand(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MaterialButton(
+              onPressed: () {
+                setState(() {
+                  launchUrl(Uri.https('hussainmustafa.com'));
+                });
+              },
+              child: Text(
+                "Launch Web Url",
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+              color: Colors.green,
+            ),
+            MaterialButton(
+              onPressed: () {
+                launchUrl(Uri.parse(
+                    "mailto:muneerhassan5544@gmail.com?subject=Flutter Developer&body=Hi How are you"));
+              },
+              child: Text(
+                "Launch Mail Url",
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+              color: Colors.red,
+            )
+          ],
+        ),
       ),
     );
   }
